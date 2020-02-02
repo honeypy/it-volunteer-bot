b5c04d4ed9c381f4b02572e36f1b9acdff59e3fc
vitaly.esipov <myuglyvenus>
2020-02-02 20:14:33 +0300

    update job queue

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 from telegram.ext import Updater, Job
+import telegram.ext
 import logging
 import config
 import sqlite3
 
+
 telegram_token = config.telegram_token
-channel = config.channel
+channel = config.test_channel
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-updater = Updater(telegram_token)
+
+updater = Updater(telegram_token, use_context= True)
 jobber = updater.job_queue
 
 db = sqlite3.connect('itvolunteer.db', check_same_thread=False)
 cursor = db.cursor()
 
-def post(bot, job):
+def post(context: telegram.ext.CallbackContext):
+    print('try to post')
     cursor.execute('SELECT * FROM tasks WHERE published=0')
     tasks_to_post = cursor.fetchall()
     for task in tasks_to_post:
         text = make_text(task)
-        bot.sendMessage(chat_id=channel, text=text, parse_mode='HTML')
+        context.bot.sendMessage(chat_id=channel, text=text, parse_mode='HTML')
         cursor.execute('UPDATE tasks SET published=(1) WHERE (link)=(?)', (task[-2],))
         db.commit()
 
@@ -25,7 +29,7 @@ def post(bot, job):
     news_to_post = cursor.fetchall()
     for n in news_to_post:
         link = n[1]
-        bot.sendMessage(chat_id=channel, text=link, parse_mode='HTML')
+        context.bot.sendMessage(chat_id=channel, text=link, parse_mode='HTML')
         cursor.execute('UPDATE news SET published=(1) WHERE (link)=(?)', (link,))
         db.commit()
 
@@ -42,12 +46,12 @@ def make_text(task):
     text = title_text+title+tags_text+tags+reward_text+reward+more_text+more
     return text
 
-job_post = Job(post, 10.0)
-jobber.put(job_post, next_t=0.0)
+job_post = jobber.run_repeating(post, interval=60, first=0)
+
 
 if __name__ == '__main__':
-    # updater.start_polling()
-    # updater.idle()
-    updater.start_webhook(listen='127.0.0.1', port=5001, url_path=telegram_token)
-    updater.bot.setWebhook(webhook_url='https://95.85.37.72/' + telegram_token, certificate=open('cert.pem', 'rb'))
-    #print('go')
+    updater.start_polling()
+    print('go')
+    updater.idle()
+    # updater.start_webhook(listen='127.0.0.1', port=5001, url_path=telegram_token)
+    # updater.bot.setWebhook(webhook_url='https://95.85.37.72/' + telegram_token, certificate=open('cert.pem', 'rb'))
